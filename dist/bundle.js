2636cd495ad977b38719ed7e3bc747edcf2900b6
Ghom <camille.abella@hotmail.fr>
2021-03-11 09:18:36 +0100

    Initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,18 @@
+var app = (() => {
+  var __defProp = Object.defineProperty;
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, {get: all[name], enumerable: true});
+  };
+
+  // src/index.ts
+  var src_exports = {};
+  __export(src_exports, {
+    hello: () => hello
+  });
+  function hello() {
+    return "Hello World!";
+  }
+  return src_exports;
+})();
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsic3JjL2luZGV4LnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJleHBvcnQgZnVuY3Rpb24gaGVsbG8oKXtcclxuICByZXR1cm4gXCJIZWxsbyBXb3JsZCFcIjtcclxufSJdLAogICJtYXBwaW5ncyI6ICI7Ozs7Ozs7O0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBTztBQUNMLFdBQU87QUFBQTsiLAogICJuYW1lcyI6IFtdCn0K
---
+++ readme.md
@@ -0,0 +1,7 @@
+# ESBuild Template for Browser
+
+- Gulp
+- TypeScript
+- Browser reloading
+
+[Use this template](https://github.com/ESBuildTemplates/ts-browser/generate)
