854ebccc3b8f99b05cc48d4cb39b5a53d7624830
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-11-14 16:07:15 +0100

    install tailwindcss and shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Avatar, AvatarImage, AvatarFallback } from "~/components/ui/avatar";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Avatar>
+        <AvatarImage src="/static/profile-picture.png" alt="@GhomKrosmonaute" />
+        <AvatarFallback>JD</AvatarFallback>
+      </Avatar>
     </div>
   );
 }
-
-export default App;
