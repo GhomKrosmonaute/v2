87c89a4bf9b31f714889c2e4fdb9558a25f59c25
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-11-15 12:05:40 +0100

    added input & fixed alias imports with craco

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-import Avatar from "./components/Avatar";
+import Avatar from "./app/Avatar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function App() {
   return (
     <div>
       <Avatar />
+      <Button>Test</Button>
+      <Input />
     </div>
   );
 }
---
+++ src/app/Avatar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import {
+  Avatar as AvatarUI,
+  AvatarImage,
+  AvatarFallback,
+} from "@/components/ui/avatar";
+
+import pp from "@assets/profile-picture.png";
+
+export default function Avatar() {
+  return (
+    <AvatarUI>
+      <AvatarImage src={pp} alt="@GhomKrosmonaute" />
+      <AvatarFallback>Ghom</AvatarFallback>
+    </AvatarUI>
+  );
+}
