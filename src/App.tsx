2bab9d8bd3abaf9b856e546c3bf2a67332b9849e
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-11-15 13:08:37 +0100

    added dark theme and toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from "react"
 
-import Avatar from "./app/Avatar"
+import Avatar from "@/app/Avatar"
+import ThemeProvider from "@/app/ThemeProvider"
+import ThemeToggle from "@/app/ThemeToggle"
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 export default function App() {
   return (
-    <div>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
       <Avatar />
       <Button>Test</Button>
       <Input />
-    </div>
+      <ThemeToggle />
+    </ThemeProvider>
   )
 }
