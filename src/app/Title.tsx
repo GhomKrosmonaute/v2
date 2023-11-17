b337406e909b481de7019e8485e5bbd0094a8dd2
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-11-17 14:23:14 +0100

    fixed reponsive oftitle

--- src/app/Title.tsx
+++ src/app/Title.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Title() {
   return (
-    <h1 className="animate-hide pointer-events-none fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl md:text-7xl lg:text-9xl whitespace-nowrap z-10 text-white">
+    <h1 className="animate-hide pointer-events-none fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl lg:text-9xl whitespace-nowrap z-10 text-white">
       CAM
       <span id="i" className="inline-block">
         I
