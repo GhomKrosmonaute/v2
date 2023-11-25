4efa163a9de26dbe3167698bf8f9572f12e2f1a9
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-11-25 17:36:46 +0100

    added columns for about-me page

--- src/tabs/AboutMe.tsx
+++ src/tabs/AboutMe.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { AlertTriangle } from "lucide-react"
 import { Alert, AlertTitle } from "@/components/ui/alert"
+import { Separator } from "@/components/ui/separator"
 
 export default function AboutMe() {
   return (
@@ -11,146 +12,162 @@ export default function AboutMe() {
           <AlertTriangle className="h-4 w-4" />
           <AlertTitle>Work in progress</AlertTitle>
         </Alert>
-        <h2 className="text-3xl my-3">A propos de moi</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
-          delectus doloremque dolores fuga maiores modi molestiae nam, omnis
-          pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
-          dolores non tempore veritatis? Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Accusamus aliquam delectus dicta earum excepturi
-          explicabo impedit minus, necessitatibus obcaecati quia sed sunt.
-          Dolore dolores laboriosam officia quasi quia repudiandae tempora?
-        </p>
-        <h2 className="text-3xl my-3">Mon parcours scolaire</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
-          delectus doloremque dolores fuga maiores modi molestiae nam, omnis
-          pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
-          dolores non tempore veritatis? Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Accusamus aliquam delectus dicta earum excepturi
-          explicabo impedit minus, necessitatibus obcaecati quia sed sunt.
-          Dolore dolores laboriosam officia quasi quia repudiandae tempora?
-        </p>
-        <h2 className="text-3xl my-3">Les jeux vidéos</h2>
-        <p>
-          LES JEUX VIDEO M'ONT PASSIONNE DEPUIS MON PLUS JEUNE AGE, J'AI
-          COMMENCE A CODER EN RETRO-INGENIERIE DES JEUX VIDEO POUR LES MODIFIER
-          A MON GOUT. J'AI ENSUITE COMMENCE A CREER MES PROPRES JEUX VIDEO AVEC
-          UNITY. J'AI ENSUITE COMMENCE A CREER DES SITES INTERNET POUR MES JEUX
-          VIDEO, PUIS POUR MES PROJETS PERSONNELS. J'AI ENSUITE DECIDE DE FAIRE
-          DE MA PASSION MON METIER EN COMMENCANT UNE FORMATION DE DEVELOPPEUR
-          WEB A L'ECOLE O'CLOCK. Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Ad aut delectus doloremque dolores fuga maiores modi
-          molestiae nam, omnis pariatur perferendis tenetur veniam! Accusantium
-          commodi deleniti, dolores non tempore veritatis? Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Accusamus aliquam delectus dicta
-          earum excepturi explicabo impedit minus, necessitatibus obcaecati quia
-          sed sunt. Dolore dolores laboriosam officia quasi quia repudiandae
-          tempora?
-        </p>
-        <h2 className="text-3xl my-3">
-          Mes premiers pas dans le monde du travail
-        </h2>
-        <p>
-          PARLER ICI DU STARTUP WEEKEND, DE MON PREMIER STAGE CHEZ AGENCE POINT
-          COM Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
-          delectus doloremque dolores fuga maiores modi molestiae nam, omnis
-          pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
-          dolores non tempore veritatis? Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Accusamus aliquam delectus dicta earum excepturi
-          explicabo impedit minus, necessitatibus obcaecati quia sed sunt.
-          Dolore dolores laboriosam officia quasi quia repudiandae tempora?
-        </p>
-        <h2 className="text-3xl my-3">PlayCurious</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
-          delectus doloremque dolores fuga maiores modi molestiae nam, omnis
-          pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
-          dolores non tempore veritatis? Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Accusamus aliquam delectus dicta earum excepturi
-          explicabo impedit minus, necessitatibus obcaecati quia sed sunt.
-          Dolore dolores laboriosam officia quasi quia repudiandae tempora?
-        </p>
-        <h2 className="text-3xl my-3">Mes premiers gros projets</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
-          delectus doloremque dolores fuga maiores modi molestiae nam, omnis
-          pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
-          dolores non tempore veritatis? Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Accusamus aliquam delectus dicta earum excepturi
-          explicabo impedit minus, necessitatibus obcaecati quia sed sunt.
-          Dolore dolores laboriosam officia quasi quia repudiandae tempora?
-        </p>
-        <h2 className="text-3xl my-3">Ma formation</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
-          delectus doloremque dolores fuga maiores modi molestiae nam, omnis
-          pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
-          dolores non tempore veritatis? Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Accusamus aliquam delectus dicta earum excepturi
-          explicabo impedit minus, necessitatibus obcaecati quia sed sunt.
-          Dolore dolores laboriosam officia quasi quia repudiandae tempora?
-        </p>
-        <h2 className="text-3xl my-3">Les Laboratoires JS</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
-          delectus doloremque dolores fuga maiores modi molestiae nam, omnis
-          pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
-          dolores non tempore veritatis? Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Accusamus aliquam delectus dicta earum excepturi
-          explicabo impedit minus, necessitatibus obcaecati quia sed sunt.
-          Dolore dolores laboriosam officia quasi quia repudiandae tempora?
-        </p>
-        <h2 className="text-3xl my-3">Mon projet de boite de jeu vidéo</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
-          delectus doloremque dolores fuga maiores modi molestiae nam, omnis
-          pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
-          dolores non tempore veritatis? Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Accusamus aliquam delectus dicta earum excepturi
-          explicabo impedit minus, necessitatibus obcaecati quia sed sunt.
-          Dolore dolores laboriosam officia quasi quia repudiandae tempora?
-        </p>
-        <h2 className="text-3xl my-3">Mon but dans la vie</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
-          delectus doloremque dolores fuga maiores modi molestiae nam, omnis
-          pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
-          dolores non tempore veritatis? Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Accusamus aliquam delectus dicta earum excepturi
-          explicabo impedit minus, necessitatibus obcaecati quia sed sunt.
-          Dolore dolores laboriosam officia quasi quia repudiandae tempora?
-        </p>
-        <h2 className="text-3xl my-3">Ma philosophie</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-          voluptatum, quia, voluptatem, quibusdam voluptates voluptate
-          voluptatibus doloremque quos quod laboriosam. Quisquam, voluptatum
-          quibusdam. Quisquam, voluptatum quibusdam. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Quae voluptatum, quia, voluptatem,
-          quibusdam voluptates voluptate voluptatibus doloremque quos quod
-          laboriosam. Quisquam, voluptatum quibusdam. Quisquam, voluptatum
-          quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Quae voluptatum, quia, voluptatem, quibusdam voluptates voluptate
-          voluptatibus doloremque quos quod laboriosam. Quisquam, voluptatum
-          quibusdam. Quisquam, voluptatum quibusdam. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Quae voluptatum, quia, voluptatem,
-          quibusdam voluptates voluptate voluptatibus doloremque quos quod
-          laboriosam. Quisquam, voluptatum quibusdam. Quisquam, voluptatum
-          quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Quae voluptatum, quia, voluptatem, quibusdam voluptates voluptate
-          voluptatibus doloremque quos quod laboriosam. Quisquam, voluptatum
-          quibusdam. Quisquam, voluptatum quibusdam. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Quae voluptatum, quia, voluptatem,
-          quibusdam voluptates voluptate voluptatibus doloremque quos quod
-          laboriosam. Quisquam, voluptatum quibusdam. Quisquam, voluptatum
-          quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Quae voluptatum, quia, voluptatem, quibusdam voluptates voluptate
-          voluptatibus doloremque quos quod laboriosam. Quisquam, voluptatum
-          quibusdam. Quisquam, voluptatum quibusdam. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Quae voluptatum, quia, voluptatem,
-          quibus
-        </p>
+        <div id="redaction" className="block xl:flex xl:gap-3">
+          <div className="basis-full">
+            <h2>A propos de moi</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
+              delectus doloremque dolores fuga maiores modi molestiae nam, omnis
+              pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
+              dolores non tempore veritatis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusamus aliquam delectus dicta
+              earum excepturi explicabo impedit minus, necessitatibus obcaecati
+              quia sed sunt. Dolore dolores laboriosam officia quasi quia
+              repudiandae tempora?
+            </p>
+            <h2>Mon parcours scolaire</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
+              delectus doloremque dolores fuga maiores modi molestiae nam, omnis
+              pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
+              dolores non tempore veritatis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusamus aliquam delectus dicta
+              earum excepturi explicabo impedit minus, necessitatibus obcaecati
+              quia sed sunt. Dolore dolores laboriosam officia quasi quia
+              repudiandae tempora?
+            </p>
+            <h2>Les jeux vidéos</h2>
+            <p>
+              LES JEUX VIDEO M'ONT PASSIONNE DEPUIS MON PLUS JEUNE AGE, J'AI
+              COMMENCE A CODER EN RETRO-INGENIERIE DES JEUX VIDEO POUR LES
+              MODIFIER A MON GOUT. J'AI ENSUITE COMMENCE A CREER MES PROPRES
+              JEUX VIDEO AVEC UNITY. J'AI ENSUITE COMMENCE A CREER DES SITES
+              INTERNET POUR MES JEUX VIDEO, PUIS POUR MES PROJETS PERSONNELS.
+              J'AI ENSUITE DECIDE DE FAIRE DE MA PASSION MON METIER EN
+              COMMENCANT UNE FORMATION DE DEVELOPPEUR WEB A L'ECOLE O'CLOCK.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
+              delectus doloremque dolores fuga maiores modi molestiae nam, omnis
+              pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
+              dolores non tempore veritatis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusamus aliquam delectus dicta
+              earum excepturi explicabo impedit minus, necessitatibus obcaecati
+              quia sed sunt. Dolore dolores laboriosam officia quasi quia
+              repudiandae tempora?
+            </p>
+            <h2>Mes premiers pas dans le monde du travail</h2>
+            <p>
+              PARLER ICI DU STARTUP WEEKEND, DE MON PREMIER STAGE CHEZ AGENCE
+              POINT COM Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit. Ad aut delectus doloremque dolores fuga maiores modi
+              molestiae nam, omnis pariatur perferendis tenetur veniam!
+              Accusantium commodi deleniti, dolores non tempore veritatis? Lorem
+              ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+              aliquam delectus dicta earum excepturi explicabo impedit minus,
+              necessitatibus obcaecati quia sed sunt. Dolore dolores laboriosam
+              officia quasi quia repudiandae tempora?
+            </p>
+            <h2>PlayCurious</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
+              delectus doloremque dolores fuga maiores modi molestiae nam, omnis
+              pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
+              dolores non tempore veritatis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusamus aliquam delectus dicta
+              earum excepturi explicabo impedit minus, necessitatibus obcaecati
+              quia sed sunt. Dolore dolores laboriosam officia quasi quia
+              repudiandae tempora?
+            </p>
+            <h2>Mes premiers gros projets</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
+              delectus doloremque dolores fuga maiores modi molestiae nam, omnis
+              pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
+              dolores non tempore veritatis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusamus aliquam delectus dicta
+              earum excepturi explicabo impedit minus, necessitatibus obcaecati
+              quia sed sunt. Dolore dolores laboriosam officia quasi quia
+              repudiandae tempora?
+            </p>
+          </div>
+          <Separator className="hidden xl:block" orientation="vertical" />
+          <div className="basis-full">
+            <h2>Ma formation</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
+              delectus doloremque dolores fuga maiores modi molestiae nam, omnis
+              pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
+              dolores non tempore veritatis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusamus aliquam delectus dicta
+              earum excepturi explicabo impedit minus, necessitatibus obcaecati
+              quia sed sunt. Dolore dolores laboriosam officia quasi quia
+              repudiandae tempora?
+            </p>
+            <h2>Les Laboratoires JS</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
+              delectus doloremque dolores fuga maiores modi molestiae nam, omnis
+              pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
+              dolores non tempore veritatis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusamus aliquam delectus dicta
+              earum excepturi explicabo impedit minus, necessitatibus obcaecati
+              quia sed sunt. Dolore dolores laboriosam officia quasi quia
+              repudiandae tempora?
+            </p>
+            <h2>Mon projet de boite de jeu vidéo</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
+              delectus doloremque dolores fuga maiores modi molestiae nam, omnis
+              pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
+              dolores non tempore veritatis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusamus aliquam delectus dicta
+              earum excepturi explicabo impedit minus, necessitatibus obcaecati
+              quia sed sunt. Dolore dolores laboriosam officia quasi quia
+              repudiandae tempora?
+            </p>
+            <h2>Mon but dans la vie</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut
+              delectus doloremque dolores fuga maiores modi molestiae nam, omnis
+              pariatur perferendis tenetur veniam! Accusantium commodi deleniti,
+              dolores non tempore veritatis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusamus aliquam delectus dicta
+              earum excepturi explicabo impedit minus, necessitatibus obcaecati
+              quia sed sunt. Dolore dolores laboriosam officia quasi quia
+              repudiandae tempora?
+            </p>
+            <h2>Ma philosophie</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              voluptatum, quia, voluptatem, quibusdam voluptates voluptate
+              voluptatibus doloremque quos quod laboriosam. Quisquam, voluptatum
+              quibusdam. Quisquam, voluptatum quibusdam. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Quae voluptatum, quia,
+              voluptatem, quibusdam voluptates voluptate voluptatibus doloremque
+              quos quod laboriosam. Quisquam, voluptatum quibusdam. Quisquam,
+              voluptatum quibusdam. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quae voluptatum, quia, voluptatem, quibusdam
+              voluptates voluptate voluptatibus doloremque quos quod laboriosam.
+              Quisquam, voluptatum quibusdam. Quisquam, voluptatum quibusdam.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              voluptatum, quia, voluptatem, quibusdam voluptates voluptate
+              voluptatibus doloremque quos quod laboriosam. Quisquam, voluptatum
+              quibusdam. Quisquam, voluptatum quibusdam. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Quae voluptatum, quia,
+              voluptatem, quibusdam voluptates voluptate voluptatibus doloremque
+              quos quod laboriosam. Quisquam, voluptatum quibusdam. Quisquam,
+              voluptatum quibusdam. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quae voluptatum, quia, voluptatem, quibusdam
+              voluptates voluptate voluptatibus doloremque quos quod laboriosam.
+              Quisquam, voluptatum quibusdam. Quisquam, voluptatum quibusdam.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              voluptatum, quia, voluptatem, quibusdam voluptates voluptate
+              voluptatibus doloremque quos quod laboriosam. Quisquam, voluptatum
+              quibusdam. Quisquam, voluptatum quibusdam. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Quae voluptatum, quia,
+              voluptatem, quibus
+            </p>
+          </div>
+        </div>
       </div>
     </ScrollArea>
   )
--- src/tabs/Pricing.tsx
+++ src/tabs/Pricing.tsx
@@ -11,6 +11,12 @@ export default function Pricing() {
           <AlertTriangle className="h-4 w-4" />
           <AlertTitle>Work in progress</AlertTitle>
         </Alert>
+        <div className="hidden">
+          todo: trouver une source fiable pour calculer dynamiquement mes tarifs
+          à l'heure en fonction des tarifs moyens en France. Afficher un
+          graphique avec une zone représentant ou je me situe par rapport aux
+          autres en fonction de mon expérience.
+        </div>
       </div>
     </ScrollArea>
   )
