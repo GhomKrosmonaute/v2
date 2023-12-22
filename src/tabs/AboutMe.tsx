7d86367a1c57760545b2c1ce48c93b336b36a699
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-12-22 12:43:31 +0100

    updated Bio

--- src/tabs/AboutMe.tsx
+++ src/tabs/AboutMe.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 
-import { ArrowUp } from "lucide-react"
+import { ArrowUp, AlertCircle } from "lucide-react"
 
+import { Alert, AlertTitle } from "@/components/ui/alert"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Button } from "@/components/ui/button"
 
@@ -108,13 +109,13 @@ export default function AboutMe({
               headerEnabled ? "" : "opacity-100 h-auto"
             }`}
           >
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center gap-3">
               <Avatar
                 className={`w-28 h-28 inline-block scale-0 transition-all duration-200 ${
                   headerEnabled ? "" : "scale-100"
                 }`}
               />
-              <h1 className="text-4xl title">Ma biographie</h1>
+              <h1 className="text-4xl title">Ma Bio</h1>
             </div>
           </div>
           <div
@@ -160,13 +161,26 @@ export default function AboutMe({
                 contacter pour discuter de la manière dont nous pourrions
                 travailler ensemble pour concrétiser votre vision en ligne.
               </p>
+              <Alert>
+                <AlertCircle className="h-5 w-5" />
+                <AlertTitle>
+                  Pour m'engager, rendez-vous sur mon profile&nbsp;
+                  <a
+                    href="https://www.malt.fr/profile/camilleabella"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Malt.fr
+                  </a>
+                </AlertTitle>
+              </Alert>
             </div>
             <div>
               <h2>Mon parcours scolaire</h2>
               <p>
-                Mon parcours scolaire a été tout sauf conventionnel, marqué par
-                une agilité d'esprit et une quête constante de créativité. Tout
-                se déroulait normalement jusqu'en 4ème, où mon esprit agité et
+                Mon parcours scolaire a été tout sauf classique, marqué par une
+                agilité d'esprit et une quête constante de créativité. Tout se
+                déroulait normalement jusqu'en 4ème, où mon esprit agité et
                 espiègle me caractérisait comme un véritable garnement.
                 Cependant, dès cette période, une remise en question profonde du
                 système éducatif m'a poussé à prendre des chemins moins
@@ -197,10 +211,9 @@ export default function AboutMe({
                 Mon parcours atypique témoigne de ma conviction profonde selon
                 laquelle l'apprentissage est une aventure personnelle, et le
                 développement de compétences ne se limite pas nécessairement aux
-                salles de classe conventionnelles. Aujourd'hui, fort de cette
-                expérience hors du commun, je mets en pratique ces leçons
-                apprises dans chaque projet, apportant une touche unique et
-                novatrice à chaque ligne de code que je crée.
+                salles de classe. Aujourd'hui, fort de cette expérience hors du
+                commun, je mets en pratique ces leçons apprises dans chaque
+                projet.
               </p>
             </div>
             <div>
@@ -208,34 +221,118 @@ export default function AboutMe({
               <p>
                 Les jeux vidéo ont été une source inépuisable de passion et
                 d'inspiration tout au long de ma vie, jouant un rôle déterminant
-                dans la manière dont je conçois le codage. Dès mon plus jeune
-                âge, ma fascination pour les jeux vidéo a été le catalyseur qui
-                m'a amené à explorer le monde complexe du développement
-                informatique. Mon parcours a débuté par la rétro-ingénierie de
-                jeux vidéo, où j'ai entrepris de modifier et d'adapter ces
-                univers virtuels à mes propres préférences, fusionnant ainsi ma
-                passion pour le jeu avec celle du codage.
+                dans la manière dont je conçois la programmation. Dès mon plus
+                jeune âge, ma fascination pour les jeux vidéo a été le
+                catalyseur qui m'a amené à explorer le monde complexe du
+                développement informatique. Mon parcours a débuté par la
+                rétro-ingénierie de jeux vidéo, où j'ai entrepris de modifier et
+                d'adapter ces univers virtuels à mes propres préférences,
+                fusionnant ainsi ma passion pour le jeu avec celle du code.
               </p>
               <p>
                 À mesure que ma compréhension du développement s'affinait, j'ai
                 naturellement évolué vers la création de mes propres jeux vidéo
                 par pur plaisir. Cette transition a été nourrie par l'influence
                 de titres emblématiques qui ont laissé une empreinte indélébile
-                sur ma vision du codage.
+                sur mon vécu. En voici quelques uns:
               </p>
+              <ul>
+                <li>
+                  <strong>Dofus</strong> - Premier MMORPG français, gameplay
+                  tactique
+                </li>
+                <li>
+                  <strong>Krosmaga</strong> - Mix entre un jeu de carte et un
+                  jeu de plateau
+                </li>
+                <li>
+                  <strong>Advance Wars</strong> - Jeu de tactique de guerre sur
+                  un damier
+                </li>
+                <li>
+                  <strong>Fire Emblem</strong> - Advance Wars mais en médieval
+                </li>
+                <li>
+                  <strong>Flight Simulator</strong> - Simulateur de vol avec une
+                  histoire
+                </li>
+                <li>
+                  <strong>Saga Mario & Luigi</strong> - RPG tactique de Nintendo
+                </li>
+                <li>
+                  <strong>Pokemon</strong> - RPG tactique de collection
+                </li>
+                <li>
+                  <strong>The Binding of Isaac</strong> - Mon premier Rogue-like
+                </li>
+                <li>
+                  <strong>Halo 3 ODST</strong> - FPS, double histoire rythmée
+                  par des phases d'aventure et d'enquête
+                </li>
+                <li>
+                  <strong>Call of Duty: WaW</strong> - FPS, premier mode zombie,
+                  adventure historique
+                </li>
+                <li>
+                  <strong>Minitroopers</strong> - Combat automatique, équipe
+                  contre équipe
+                </li>
+                <li>
+                  <strong>La Brute</strong> - Combat automatique, joueur contre
+                  joueur
+                </li>
+                <li>
+                  <strong>Minecraft</strong> - Jeu bac à sable, RPG de survie
+                </li>
+                <li>
+                  <strong>Mario Kart</strong> - Jeu de course type arcade
+                </li>
+              </ul>
               <p>
                 Ah, les souvenirs d'enfance sont souvent teintés d'une touche
                 espiègle. Il y a une anecdote qui résonne particulièrement dans
-                ma mémoire ludique. Lorsque j'étais gosse, un ami généreux
-                m'avait prêté le jeu "Advance Wars" sur Nintendo DS. C'était une
-                immersion captivante dans le monde de la stratégie militaire, et
-                je me suis tellement plongé dans l'univers du jeu que, je dois
+                ma mémoire. Lorsque j'étais gosse, un ami généreux m'avait prêté
+                le jeu "Advance Wars" sur Nintendo DS. C'était une immersion
+                captivante dans le monde de la stratégie militaire, et je me
+                suis tellement plongé dans l'univers du jeu que, je dois
                 l'avouer avec un sourire complice, je n'ai jamais trouvé le
                 chemin du retour pour rendre le jeu à son propriétaire. Un petit
-                acte espiègle qui a peut-être forgé ma passion pour les mondes
-                virtuels et les intrigues stratégiques. Après tout, certains
-                jeux ont simplement un pouvoir irrésistible. Désolé, cher ami,
-                si tu lis ceci. C'était une aventure mémorable! 😄
+                acte qui a peut-être forgé ma passion pour le genre tactical.
+                Désolé, cher ami, si tu lis ceci. C'était une aventure
+                mémorable! 😄
+              </p>
+              <p>
+                Aujourd'hui encore, je continue d'être passionné par tous ces
+                nouveaux types de jeux qui sortent chaque mois. Dernièrement je
+                me suis notamment intéressé à certains titres innovants tels
+                que:
+              </p>
+              <ul>
+                <li>
+                  <strong>Inscryption</strong> - Mix entre jeu de carte, jeu
+                  d'enquête et Rogue-like
+                </li>
+                <li>
+                  <strong>Despot's Game</strong> - Rogue like en combat
+                  automatique, jeu des synergies
+                </li>
+                <li>
+                  <strong>Waven</strong> - Tactical RPG, jeu de carte et jeu de
+                  plateau. Successeur de Dofus
+                </li>
+                <li>
+                  <strong>Mario et les lapins crétins</strong> - Tactical RPG à
+                  la X-COM
+                </li>
+                <li>
+                  <strong>Slay the Spire</strong> - Jeu de carte stratégique
+                  type Rogue like à choix de synergie
+                </li>
+              </ul>
+              <p>
+                Je me lance bientôt dans le développement de mon propre jeu
+                vidéo, je m'inspirerais bien entendu de tous ces jeux qui m'ont
+                marqué. Je suis impatient de partager cette aventure avec vous!
               </p>
             </div>
             <div>
@@ -243,9 +340,13 @@ export default function AboutMe({
               <p>
                 Ma vie a pris un tournant significatif lorsqu'une étape cruciale
                 m'a conduit à quitter ma période de solitude. Je me suis engagé
-                dans une aventure transformative à l'École d'Art de l'Idem, une
-                période qui a redéfini non seulement ma compréhension du code
-                mais aussi ma manière d'interagir avec le monde qui m'entoure.
+                dans une aventure transformative à l'École d'Art de&nbsp;
+                <a href="https://lidem.eu" target="_blank" rel="noreferrer">
+                  l'Idem
+                </a>
+                , une période qui a redéfini non seulement ma compréhension du
+                code mais aussi ma manière d'interagir avec le monde qui
+                m'entoure.
               </p>
               <p>
                 Au cours de ces cinq années au sein de l'école, j'ai eu
@@ -264,23 +365,45 @@ export default function AboutMe({
             <div>
               <h2>Mes premiers pas dans le monde du travail</h2>
               <p>
-                Tout a commencé lors de ma participation enthousiaste au Startup
-                Weekend de Perpignan, une expérience collaborative qui a été le
-                catalyseur de changements majeurs. Aux côtés de mon ami Arnaud,
-                nous avons formé une équipe dynamique et avons travaillé
-                intensément sur une idée novatrice. Notre projet, qui a fini par
-                se classer en 3ème place, était à l'origine de l'entreprise
-                derrière l'application de rencontre "Kerubi". L'excitation et
-                l'énergie déployées pendant ce weekend ont été récompensées par
-                l'obtention de fonds de 10 000€, qui ont permis à Arnaud de
-                concrétiser son rêve entrepreneurial. Cette expérience a été une
-                immersion profonde dans l'univers compétitif et stimulant des
-                startups, façonnant ma perspective du monde professionnel.
+                Tout a commencé lors de ma participation enthousiaste au&nbsp;
+                <a
+                  href="https://twitter.com/swperpi?lang=fr"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Startup Weekend de Perpignan
+                </a>
+                , une expérience collaborative qui a été le catalyseur de
+                changements majeurs. Aux côtés de mon ami Arnaud, nous avons
+                formé une équipe dynamique et avons travaillé intensément sur
+                une idée novatrice. Notre projet, qui a fini par se classer en
+                3ème place, était à l'origine de l'entreprise derrière
+                l'application de rencontre&nbsp;
+                <a
+                  href="https://www.lindependant.fr/2022/07/04/kerubi-une-application-de-rencontres-et-de-sorties-made-in-perpignan-10414844.php"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Kerubi
+                </a>
+                . L'excitation et l'énergie déployées pendant ce weekend ont été
+                récompensées par l'obtention de fonds de 10 000€, qui ont permis
+                à Arnaud de concrétiser son rêve entrepreneurial. Cette
+                expérience a été une immersion profonde dans l'univers
+                compétitif et stimulant des startups, façonnant ma perspective
+                du monde professionnel.
               </p>
               <p>
-                Par la suite, j'ai embrassé mon premier stage chez Agence Point
-                Com, une expérience de trois mois qui m'a permis de plonger dans
-                le monde du développement web d'une manière plus traditionnelle.
+                Par la suite, j'ai embrassé mon premier stage chez&nbsp;
+                <a
+                  href="https://agencepoint.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Agence Point Com
+                </a>
+                , une expérience de trois mois qui m'a permis de plonger dans le
+                monde du développement web d'une manière plus traditionnelle.
                 Cependant, j'ai rapidement compris que WordPress n'était pas
                 vraiment ma tasse de thé. Ce stage m'a servi de tremplin pour
                 découvrir mes préférences et mes forces en matière de
@@ -302,14 +425,21 @@ export default function AboutMe({
             <div>
               <h2>PlayCurious</h2>
               <p>
-                Quand j'ai rejoint PlayCurious, j'ai été accueilli par une
-                équipe dynamique et passionnée, qui m'a offert une immersion
-                dans l'univers du développement de jeu vidéo. J'ai eu l'occasion
-                de travailler sur une multitude de serious games, qui ont été
-                une source inépuisable d'inspiration et d'apprentissage. J'ai
-                également développé des compétences en gestion de projet, en
-                travaillant avec des équipes de développeurs et de graphistes
-                pour créer des jeux vidéo uniques.
+                Quand j'ai rejoint&nbsp;
+                <a
+                  href="https://playcurious.games"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  PlayCurious
+                </a>
+                , j'ai été accueilli par une équipe dynamique et passionnée, qui
+                m'a offert une immersion dans l'univers du développement de jeu
+                vidéo. J'ai eu l'occasion de travailler sur une multitude de
+                serious games, qui ont été une source inépuisable d'inspiration
+                et d'apprentissage. J'ai également développé des compétences en
+                gestion de projet, en travaillant avec des équipes de
+                développeurs et de graphistes pour créer des jeux vidéo uniques.
               </p>
               <p>
                 Je suis resté 5 ans dans cette boite, dont 2 ans en tant que
